0eb126a95f6772018858beaeba1791b7ec7d1844
Ken Rimple <krimple@chariotsolutions.com>
2011-04-26 21:17:13 -0400

    Pending JMS crud

--- src/main/java/org/ete/fleacircus/messaging/ChatListener.java
+++ src/main/java/org/ete/fleacircus/messaging/ChatListener.java
@@ -1,11 +1,51 @@
 package org.ete.fleacircus.messaging;
 
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.roo.addon.javabean.RooJavaBean;
 
-public class ChatListener {
+@RooJavaBean
+public class ChatListener implements MessageListener {
 
-    public void onMessage(Object message) {
-        System.out.println("JMS message received: " + message);
+	@Autowired
+	private JmsTemplate template;
+
+	Logger logger = LoggerFactory.getLogger(ChatListener.class);
+
+    String[] responses = {
+            "I'm busy combing my hair to look for, wait, me...",
+            "What are you talking about, I'm a flea?",
+            "There's food around the mountain, food around the mountain...",
+            "Are you looking for the bassist for the Red Hot Chili Peppers?",
+            "Don't look now, but I'm about to drink your blood...",
+            "IT Department.  Did you try turning it off and on again?",
+            "My response will just make you itch..."
+    };
+
+    @Override
+    public void onMessage(Message jmsMessage) {
+    	// TODO Auto-generated method stub
+    	try {
+			String message = ((TextMessage)jmsMessage).getText();
+			logger.debug("JMS message received: " + message);
+	        // right back atcha!
+	        int response = (int)(Math.random() * (responses.length - 1));
+	        template.convertAndSend(response);
+			
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			template.convertAndSend("Error in JMS. " + e.toString());
+		}
     }
+
+
 }
--- src/main/java/org/ete/fleacircus/web/ChatController.java
+++ src/main/java/org/ete/fleacircus/web/ChatController.java
@@ -2,31 +2,36 @@ package org.ete.fleacircus.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
-import org.ete.fleacircus.model.MessageCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/chat/**")
 @Controller
 public class ChatController {
 
     @Autowired
-    @Qualifier("chatDestination")
     private JmsTemplate template;
-
 
     public void get(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public void sendMessage(Model model, @Valid MessageCommand message, BindingResult errors) {
-        template.convertAndSend();
+    public @ResponseBody String sendMessage(ModelMap model, @RequestParam String message, BindingResult errors) {
+        template.convertAndSend(message);
+        template.setReceiveTimeout(100L);
+        String response = (String) template.receiveAndConvert();
+        if (response != null) {
+            return response;
+        } else {
+            return "waiting for response...";
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -35,15 +40,13 @@ public class ChatController {
     }
 
     /**
-     * Our AJAX server method - use JSON to get updates from the MessageCommand,
-     * which we annotate with the @RooJson annotation to make easy to work with. 
+     * Our AJAX server method
      */
     @RequestMapping(method = RequestMethod.GET, value = "/updates")
     public @ResponseBody String getUpdates() {
-        String result = template.receive();
-        MessageCommand command = new MessageCommand();
-        command.setMessage(result);
-        return command.toJson();
+        template.setReceiveTimeout(1000L);
+        String result = (String) template.receiveAndConvert();
+        return result == null ? "" : result;
     }
 
 }
